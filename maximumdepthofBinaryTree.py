2fff1595841d09590404fbb7799eab664f7c4bd7
pateb9298 <102432593+pateb9298@users.noreply.github.com>
2025-07-16 01:14:30 -0400

    Update maximumdepthofBinaryTree.py

--- maximumdepthofBinaryTree.py
+++ maximumdepthofBinaryTree.py
@@ -1,3 +1,4 @@
+#ITERATIVE DFS
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -21,5 +22,22 @@ class Solution:
 
         return max_depth
 
+#RECURSIVE DFS
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
 
+        return 1 + max(left_depth, right_depth)
+
+#BFS
